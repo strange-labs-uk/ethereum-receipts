bab21cfac48d5423fbf4deddee6b7d99fa9c22f3
Errol Chevannes <errol@malooba.com>
2018-01-31 21:54:53 +0000

    Implemented storage contract.

--- truffle/contracts/Storage.sol
+++ truffle/contracts/Storage.sol
@@ -2,6 +2,46 @@ pragma solidity ^0.4.18;
 
 contract Storage {
 
+    address private owner;
+    address public controller;
     mapping(string => string) private values;
+
+    event ValueWritten(string _key);
+    event ControllerUpdated(address _controller);
+
+    function Storage(address _controller) 
+    public 
+    {
+        owner = msg.sender;
+        controller = _controller;
+    }
+
+    function write (
+        string _key, 
+        string _value) 
+        public 
+        {
+            require(msg.sender == controller); 
+            values[_key] = _value;
+            ValueWritten(_key);
+        }
+
+    function read (
+        string _key)
+        public
+        view
+        returns (string _value)
+        {
+            require(msg.sender == controller); 
+            return values[_key];
+        }
     
+    function updateController (
+        address _controller)
+        public
+        {
+            require(msg.sender == owner); 
+            controller = _controller;
+            ControllerUpdated(controller);
+        }
 }
--- truffle/test/Storage.js
+++ truffle/test/Storage.js
@@ -2,48 +2,98 @@ var Storage = artifacts.require("Storage");
 
 contract('Storage', function(accounts) {
 
-    it("should create the value if the key does not exist", (done) => {
-        assert.fail();
+    //////////////////////////////
+    // Writing values
+    //////////////////////////////
+
+    it("should write the value if the key does not exist", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx = await storage.write("key", "value");
+        assert.equal(1, tx.receipt.status, "should have created value");
     }); 
 
-    it("should update the value if the key does exist", (done) => {
-        assert.fail();
+    it("should overwrite the value if the key already exists", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx1 = await storage.write("key", "value");
+        var tx2 = await storage.write("key", "value");
+        assert.equal(1, tx1.receipt.status, "should have created value");
+        assert.equal(1, tx2.receipt.status, "should have created value");
     }); 
 
-    it("should get the value if teh key does exist", (done) => {
-        assert.fail();
-    }); 
-
-    it("should delete the value if the key does exist", (done) => {
-        assert.fail();
+    it("should emit a 'ValueWritten' event when a value is written", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx = await storage.write("key", "value");
+        assert.equal("ValueWritten", tx.logs[0].event, "event should have been emitted")
     });
 
-    it("should return an error if the key to update does not exist", (done) => {
-        assert.fail();
-    }); 
-
-    it("should return an error if the key to delete does not exist", (done) => {
-        assert.fail();
-    }); 
-
-    it("should update the controller address if called by the owning address", (done) => {
-        assert.fail();
-    }); 
-
-    it("should not update the controller address if not called by the owning address", (done) => {
-        assert.fail();
-    }); 
-
-    it("should emit a 'ValueCreated' event when a value is created", (done) => {
-        assert.fail();
+    it("should revert when 'write' is not called by the owning address", async () => {
+        let threw = false;
+        var storage = await Storage.new(accounts[1]);
+        try {
+            await storage.write("key", "value");    
+        } catch (error) {
+            assert.include(error.message, 'revert', 'should revert');
+            threw = true;
+        }
+        assert.isTrue(threw, "should have thrown exception");
     });
 
-    it("should emit a 'ValueUpdated' event when a value is updated", (done) => {
-        assert.fail();
+    //////////////////////////////
+    // Reading values
+    //////////////////////////////
+
+    it("should read the value if the key exists", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx1 = await storage.write("key", "value");
+        var value =  await storage.read("key");
+        assert.equal("value", value, "should have read value");
+    }); 
+
+    it("should return an empty value if the key does not exist", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx1 = await storage.write("key", "value");
+        var value =  await storage.read("nokey");
+        assert.equal("", value, "should have read value");
+    }); 
+
+    it("should revert when 'read' is not called by the owning address", async () => {
+        let threw = false;
+        var storage = await Storage.new(accounts[1]);
+        try {
+            await storage.read("key");
+        } catch (error) {
+            assert.include(error.message, 'revert', 'should revert');
+            threw = true;
+        }
+        assert.isTrue(threw, "should have thrown exception");
     });
 
-    it("should emit a 'ValueDeleted' event when a value is deleted", (done) => {
-        assert.fail();
+    //////////////////////////////
+    // Updating the controller
+    //////////////////////////////
+    
+    it("should update the controller if called by the owning address", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx = await storage.updateController(accounts[1]);
+        var controller = await storage.controller.call();
+        assert.equal(accounts[1], controller, "should have updated controller");
+    }); 
+
+    it("should emit a 'ControllerUpdated' event when the controller is updated", async () => {
+        var storage = await Storage.new(accounts[0]);
+        var tx = await storage.updateController(accounts[1]);
+        assert.equal("ControllerUpdated", tx.logs[0].event, "event should have been emitted")
     });
 
+    it("should revert when 'updateController' is not called by the owning address", async () => {
+        let threw = false;
+        var storage = await Storage.new(accounts[2], {from: accounts[1]});
+        try {
+            var tx = await storage.updateController(accounts[3]);
+        } catch (error) {
+            assert.include(error.message, 'revert', 'should revert');
+            threw = true;
+        }
+        assert.isTrue(threw, "should have thrown exception");
+    }); 
 });
